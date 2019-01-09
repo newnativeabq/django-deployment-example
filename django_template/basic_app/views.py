424f805638845f9cf793b7f9e454998a03514e82
Vincent B <vincent@itouchup.org>
2019-01-09 10:02:23 -0700

    Updated with basic class-based view

--- django_template/basic_app/views.py
+++ django_template/basic_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.http import HttpResponse
 
 # Create your views here.
+
+#Function-based view
+# def index(request):
+#     return render(request, 'index.html')
+
+#Class-based view
+class CBView(View):
+    def get(self, request):
+        return HttpResponse("Class Based Views are COOL!")
